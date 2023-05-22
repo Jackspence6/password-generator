83aa5153e4faffb0e2762d46202862d4dc9786e4
Jackspence6 <jackspence123456@gmail.com>
2023-05-22 13:15:35 +0200

    Added password creation function.

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 /* --------------------------------- password character options --------------------------------- */
@@ -17,6 +16,60 @@ const passwordOptions = {
   numericCharacters: "1234567890",
   specialCharacters: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
 };
+
+/* -------------------------------------- initial password -------------------------------------- */
+var initialPassword = "";
+
+/* ------------------------------------- questions for users ------------------------------------ */
+
+const passwordLength = window.prompt(
+  "how many characters do you want your password to be?"
+);
+const lCaseChars = window.confirm(
+  "Do you want to include lowerCase characters?"
+);
+const uCaseChars = window.confirm(
+  "Do you want to include upperCase characters?"
+);
+const nums = window.confirm("Do you want to include numbers?");
+const sChars = window.confirm("Do you want to include special characters?");
+
+if (passwordLength >= 8 && passwordLength <= 128) {
+  if (lCaseChars) {
+    console.log("yes");
+    initialPassword = initialPassword.concat(
+      passwordOptions.lowercaseCharacters
+    );
+    console.log(initialPassword);
+  } else {
+    console.log("no");
+  }
+  if (uCaseChars) {
+    console.log("yes");
+    initialPassword = initialPassword.concat(
+      passwordOptions.uppercaseCharacters
+    );
+    console.log(initialPassword);
+  } else {
+    console.log("no");
+  }
+  if (nums) {
+    console.log("yes");
+    initialPassword = initialPassword.concat(passwordOptions.numericCharacters);
+    console.log(initialPassword);
+  } else {
+    console.log("no");
+  }
+  if (sChars) {
+    console.log("yes");
+    initialPassword = initialPassword.concat(passwordOptions.specialCharacters);
+    console.log(initialPassword);
+  } else {
+    console.log("no");
+  }
+
+  console.log(initialPassword);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
