8fbceabbdde4b300063963eb0aa792195724e5ab
Jackspence6 <jackspence123456@gmail.com>
2023-05-23 13:53:18 +0200

    Solved bug that prevents password from displaying on webpage.

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,110 +1,116 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+/* -------------------- Declaring result4 outside the writePassword function -------------------- */
+var result4;
+
+/* ----------------- Function that deploys when "generate password" is clicked ---------------- */
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  /* --------------------------------- password character options --------------------------------- */
+  const passwordOptions = {
+    lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz",
+    uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numericCharacters: "1234567890",
+    specialCharacters: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+  };
 
-  passwordText.value = password;
-}
+  /* -------------------------------------- initial password -------------------------------------- */
+  var initialPassword = "";
 
-/* --------------------------------- password character options --------------------------------- */
-const passwordOptions = {
-  lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz",
-  uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numericCharacters: "1234567890",
-  specialCharacters: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
-};
+  /* ------------------------------------- questions for users ------------------------------------ */
+  const passwordLength = window.prompt(
+    "how many characters do you want your password to be?"
+  );
+  const lCaseChars = window.confirm(
+    "Do you want to include lowerCase characters?"
+  );
+  const uCaseChars = window.confirm(
+    "Do you want to include upperCase characters?"
+  );
+  const nums = window.confirm("Do you want to include numbers?");
+  const sChars = window.confirm("Do you want to include special characters?");
 
-/* -------------------------------------- initial password -------------------------------------- */
-var initialPassword = "";
-
-/* ------------------------------------- questions for users ------------------------------------ */
-const passwordLength = window.prompt(
-  "how many characters do you want your password to be?"
-);
-const lCaseChars = window.confirm(
-  "Do you want to include lowerCase characters?"
-);
-const uCaseChars = window.confirm(
-  "Do you want to include upperCase characters?"
-);
-const nums = window.confirm("Do you want to include numbers?");
-const sChars = window.confirm("Do you want to include special characters?");
-
-/* ----------------------- if statement to ensure character count is valid ---------------------- */
-if (passwordLength >= 8 && passwordLength <= 128) {
-  createPassword();
-} else {
-  console.log("not valid count");
-}
-/* ------------------------------- function to create the password ------------------------------ */
-function createPassword() {
-  if (lCaseChars) {
-    console.log("yes");
-    initialPassword = initialPassword.concat(
-      passwordOptions.lowercaseCharacters
-    );
-    console.log(initialPassword);
+  /* ----------------------- if statement to ensure character count is valid ---------------------- */
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    createPassword();
   } else {
-    console.log("no");
+    console.log("not valid count");
   }
-  if (uCaseChars) {
-    console.log("yes");
-    initialPassword = initialPassword.concat(
-      passwordOptions.uppercaseCharacters
-    );
-    console.log(initialPassword);
-  } else {
-    console.log("no");
+  /* ------------------------------- function to create the password ------------------------------ */
+  function createPassword() {
+    if (lCaseChars) {
+      console.log("yes");
+      initialPassword = initialPassword.concat(
+        passwordOptions.lowercaseCharacters
+      );
+      console.log(initialPassword);
+    } else {
+      console.log("no");
+    }
+    if (uCaseChars) {
+      console.log("yes");
+      initialPassword = initialPassword.concat(
+        passwordOptions.uppercaseCharacters
+      );
+      console.log(initialPassword);
+    } else {
+      console.log("no");
+    }
+    if (nums) {
+      console.log("yes");
+      initialPassword = initialPassword.concat(
+        passwordOptions.numericCharacters
+      );
+      console.log(initialPassword);
+    } else {
+      console.log("no");
+    }
+    if (sChars) {
+      console.log("yes");
+      initialPassword = initialPassword.concat(
+        passwordOptions.specialCharacters
+      );
+      console.log(initialPassword);
+    } else {
+      console.log("no");
+    }
   }
-  if (nums) {
-    console.log("yes");
-    initialPassword = initialPassword.concat(passwordOptions.numericCharacters);
-    console.log(initialPassword);
-  } else {
-    console.log("no");
+
+  console.log(initialPassword);
+
+  /* ----------------------- function to add correct quantity of characters at random---------------------- */
+  function concatenateRandomPassword(str, count) {
+    var stage1PW = [];
+
+    for (let i = 0; i < count; i++) {
+      var randomIndex = Math.floor(Math.random() * str.length);
+      var randomCharacters = str[randomIndex];
+      stage1PW.push(randomCharacters);
+    }
+    return stage1PW;
   }
-  if (sChars) {
-    console.log("yes");
-    initialPassword = initialPassword.concat(passwordOptions.specialCharacters);
-    console.log(initialPassword);
-  } else {
-    console.log("no");
-  }
+
+  var result1 = concatenateRandomPassword(initialPassword, passwordLength);
+
+  console.log(result1);
+
+  /* ------------------------------------- Password formatting ------------------------------------ */
+  // Change array to string
+  var result2 = result1.toString();
+  console.log(result2);
+
+  //Remove quotes
+  var result3 = result2.replace(/'/g, "");
+
+  // Remove commas
+  var result4 = result3.replace(/,\s*/g, "");
+
+  console.log(result4);
+
+  /* ----------------------- Assigning the password to the HTML text element ---------------------- */
+   var passwordText = document.querySelector("#password");
+   passwordText.value = result4;
 }
-
-console.log(initialPassword);
-
-/* ----------------------- function to add correct quantity of characters at random---------------------- */
-function concatenateRandomPassword(str, count) {
-  var stage1PW = [];
-
-  for (let i = 0; i < count; i++) {
-    var randomIndex = Math.floor(Math.random() * str.length);
-    var randomCharacters = str[randomIndex];
-    stage1PW.push(randomCharacters);
-  }
-  return stage1PW;
-}
-
-var result1 = concatenateRandomPassword(initialPassword, passwordLength);
-
-console.log(result1);
-
-/* ------------------------------------- Password formatting ------------------------------------ */
-// Change array to string
-var result2 = result1.toString();
-console.log(result2);
-
-//Remove quotes
-var result3 = result2.replace(/'/g, "");
-
-// Remove commas
-var result4 = result3.replace(/,\s*/g, "");
-
-console.log(result4);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
